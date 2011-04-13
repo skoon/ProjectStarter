c00abb0d50fe498e1507b05553e8c610b4bf3825
Scott Koon <scott@lazycoder.com>
2011-04-12 23:15:17 -0700

    refactored tests

--- tests/when_parsing_arguments.cs
+++ tests/when_parsing_arguments.cs
@@ -1,32 +1,8 @@
 using System;
-using System.Collections.Generic;
 using Xunit;
 using ProjectStarter;
 
 namespace ProjectStarter_tests {
-
-    public class MockFileSystem : IFileSystem {
-        public string WorkingDirectory{get; set;}
-        public bool CreateDirectoryCalled = false;
-        public bool CreateFileCalled = false;
-        public bool CreateDirectoryInWorkingDirectoryCalled = false;
-
-        public List<string> directoriesCreated = new List<string>();
-        
-        public void CreateDirectory(string name) {
-            CreateDirectoryCalled = true;
-            directoriesCreated.Add(name);
-        }
-        
-        public void CreateFile(string name, string contents) {
-            CreateFileCalled = true;
-        }
-        
-        public void CreateDirectoryInWorkingDirectory(string name) {
-            CreateDirectoryInWorkingDirectoryCalled = true;
-            directoriesCreated.Add(name);
-        }
-    }
 
     public class when_parsing_arguments {
         
@@ -44,24 +20,4 @@ namespace ProjectStarter_tests {
            Assert.IsType(typeof(NewProject), parser.Command); 
         }
     }
-
-    public class when_executing_the_NewProject_command {
-        
-        public ICommand command;
-        public MockFileSystem mockFileSystem = new MockFileSystem();
-        public const string projectname = "test";
-
-        public when_executing_the_NewProject_command() {
-            var parser = new ArgParser(new string[] {projectname}, mockFileSystem); 
-            parser.Command.Execute();
-
-        }
-
-        [Fact]
-        public void the_project_directory_should_be_created() {
-            Assert.Contains(projectname, mockFileSystem.directoriesCreated);
-        }
-    
-    }
-
 }
