c01d709199d224c4c313edd8a7e0bb94277f81ae
skoon <scott@lazycoder.com>
2011-04-28 00:15:22 -0700

    added test for deserializing the config file

--- src/ArgParser.cs
+++ src/ArgParser.cs
@@ -5,7 +5,7 @@ namespace ProjectStarter
     public class ArgParser
     {
         public ICommand Command { get; set;}
-        public string Config { get; set; }
+        public ProjectStarterConfig Config { get; set; }
 
         public ArgParser(string[] args, IFileSystem FileSystem) {
             if(FileSystem == null) throw  new ArgumentNullException("FileSystem can not be null");
