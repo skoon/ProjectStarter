26161eab1401ace1121ff3e382739bc2a386317a
skoon <scott@lazycoder.com>
2011-04-28 00:12:33 -0700

    added test for deserializing the config file

--- src/ArgParser.cs
+++ src/ArgParser.cs
@@ -5,9 +5,11 @@ namespace ProjectStarter
     public class ArgParser
     {
         public ICommand Command { get; set;}
+        public string Config { get; set; }
+
         public ArgParser(string[] args, IFileSystem FileSystem) {
             if(FileSystem == null) throw  new ArgumentNullException("FileSystem can not be null");
-            var config = FileSystem.ReadFile("default.json");
+            Config = FileSystem.ReadFile("default.json");
             if(args != null) {
                 Command = new NewProject(FileSystem);
                 Command.Args = args;
