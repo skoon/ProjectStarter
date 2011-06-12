47ae60380c7c0dbf3cba805fc31af3326109fe9d
Scott Koon <scott@lazycoder.com>
2011-06-11 23:49:35 -0700

    removed an empty test class

--- tests/unittests/the_NewProject_command_should_use_the_config_file_when_creating_the_project.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Xunit;
-
-namespace ProjectStarter_Tests.UnitTests {
-    public class the_NewProject_command_should_use_the_config_file_when_creating_the_project : when_using_a_json_config_file {
-
-
-    }
-}
