8bdc13719e25a0bbe0b79222d40299970823874f
Scott Koon <scott@lazycoder.com>
2012-01-11 16:13:46 -0800

    Added integration test

--- tests/integrationtests/when_creating_a_new_project.cs
+++ tests/integrationtests/when_creating_a_new_project.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xunit;
 using ProjectStarter;
 
@@ -7,7 +8,19 @@ namespace ProjectStarter.Tests.IntegrationTests
     public class when_creating_a_new_project
     {
 
+        private ArgParser parser;
+        private string[] args;
+        private DirectoryInfoWrapper dirInfoWrapper;        
+        public when_creating_a_new_project() {
+            dirInfoWrapper = new DirectoryInfoWrapper(Environment.CurrentDirectory);
+            args = new string[] {"Test"};
+            parser = new ArgParser(args, new FileSystem( dirInfoWrapper, new FileInfoWrapper()));
+        }
+    
 
+        public void it_should_create_the_project_directory()
+        {
+            Assert.True(Directory.Exists(dirInfoWrapper.FullPath));
+        }
     }
-
 }
