2e763ebe954e9d87463698b86f2d999a0e78e388
Scott Koon <scott@lazycoder.com>
2014-11-03 12:29:24 -0800

    Added  Ninject

--- src/commands/FetchPackage.cs
+++ src/commands/FetchPackage.cs
@@ -8,10 +8,27 @@ namespace ProjectStarter
 
         public void Execute()
         {
-            throw new NotImplementedException();
+            var pathToNuget = GetPathToNuget();
+            if!(String.IsNullOrEmpty(pathToNuget)) {
+
+            } else {
+                throw new Exception("Could not find nuget.exe in your path.");
+            }
         }
 
         public string Name { get; set; }
         public IConfig Config { get; set; }
+
+    private string GetPathToNuget() {
+        var path = string.Empty;
+        var pathVar = Environment.GetEnvironmentVariable("PATH");
+        var exes = pathVar.split(";");
+        foreach(var e in exes) {
+            if(e.contains("nuget.exe")) {
+                return e;
+            }
+        }
+
+    }
     }
 }
